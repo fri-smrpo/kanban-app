f3b0c0cb4867b210da82c1ed060a75e0ad165ff6
MVolontar <44176749+MVolontar@users.noreply.github.com>
2019-03-13 22:45:24 +0100

    projetc-list

---
+++ public/js/pages/register.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+  function ajaxRegister(dataArray){
+    $.ajax({
+      url: '/v1/users',
+      type: 'POST',
+      headers: {
+        'Authorization':'Bearer ' + getToken()
+      },
+      data: JSON.stringify(dataArray),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      //async: false,
+      success: function(data) {
+        alert("Uporabnik uspešno ustvarjen!");
+        console.log(data);
+        window.location.replace("index.html");
+      },
+      error: function(response) {
+        alert("Napaka!");
+        console.log(response);
+      }
+
+    });
+
+  }
+
+  $("#anchorRegister").click(function(){
+
+    var inputFirst = $("#inputFirst");
+    var inputLast = $("#inputLast");
+
+    var inputEmail = $("#inputEmail");
+    var inputUsername = $("#inputUsername");
+
+    var inputPwd1 = $("#inputPasswd1");
+    var inputPwd2 = $("#inputPasswd2");
+
+    if(inputFirst.val() == "")
+      alert("Vnesite ime!");
+    else if(inputLast.val() == "")
+      alert("Vnesite priimek!");
+    else if(inputEmail.val() == "")
+      alert("Vnesite e-poštni naslov!");
+    else if(inputUsername.val() == "")
+      alert("Vnesite uporaniško ime!");
+    else if(inputPwd1.val() == "")
+      alert("Vnesite geslo!");
+    else if(inputPwd2.val() == "")
+      alert("Potrdite geslo!");
+    else if(inputPwd1.val() != inputPwd2.val())
+      alert("Gesli se ne ujemata!");
+    else if(inputPwd1.val().length < 6)
+      alert("Geslo naj bo dolgo vsaj 6 znakov!");
+    else if(!$("#radioUporabnik").is(":checked") && !$("#radioAdministrator").is(":checked"))
+      alert("Izberite tip uporabnika!");
+    else{
+
+      var dataArr;
+      if($("#radioUporabnik").is(":checked"))
+        dataArr = {email: inputEmail.val(),
+          password: inputPwd2.val(),
+          username: inputUsername.val(),
+          name: inputFirst.val(),
+          surname: inputLast.val()};
+      else
+        dataArr = {email: inputEmail.val(),
+          password: inputPwd2.val(),
+          username: inputUsername.val(),
+          name: inputFirst.val(),
+          surname: inputLast.val(),
+          role: 'admin'};
+      ajaxRegister(dataArr);
+
+    }
+
+  });
+});
