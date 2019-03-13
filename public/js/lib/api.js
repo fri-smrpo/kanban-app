469ec57331d1092b8e4cb1503e073b250bc3a369
Tilen Tomakić <tilen@tomakic.com>
2019-03-13 16:04:37 +0100

    Pages.

--- public/js/lib/api.js
+++ public/js/lib/api.js
@@ -12,3 +12,19 @@ function apiProjectCreate(data, success, error){
     error
   });
 }
+
+
+function apiProjectList(data, success, error){
+  $.ajax({
+    url: '/v1/projects',
+    type: 'GET',
+    headers: {
+      'Authorization':'Bearer ' + getToken()
+    },
+    data,
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success,
+    error
+  });
+}
