88c142d30460cbe8dacd10822a49e34e84a03000
MVolontar <44176749+MVolontar@users.noreply.github.com>
2019-03-13 17:15:11 +0100

    projetc-list

--- public/js/lib/api.js
+++ public/js/lib/api.js
@@ -24,14 +24,14 @@ function apiProjectCreate(data, success, error){
 }
 
 
-function apiProjectList(data, success, error){
+function apiProjectList(success, error){
   $.ajax({
     url: '/v1/projects',
     type: 'GET',
     headers: {
       'Authorization':'Bearer ' + getToken()
     },
-    data,
+
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
     success,
