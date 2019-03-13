e85b520984275272cca087275ec1d53fb6c023ec
Nenad <nenadbabic995@gmail.com>
2019-03-13 23:36:08 +0100

    Populate users, print users, remove user when selected

---
+++ public/js/pages/project-list.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  function populateList(data) {
+
+    console.log("Populating");
+    $("#dropdownUporabniki").empty();
+    var length = Object.keys(data).length;
+    console.log(length);
+    for (var i = 0; i < length; i++) {
+      $('#dropdownUporabniki').append($("<option />").val(data[i].username).text(data[i].username + ' (' + data[i].name + ' ' + data[i].surname + ')'));
+    }
+  }
+
+  function loadPeople() {
+    $.ajax({
+      url: '/v1/users',
+      type: 'GET',
+      headers: {
+        'Authorization':'Bearer ' + getToken()
+      },
+      dataType: 'json',
+      //async: false,
+      success: function(data) {
+        console.log(data);
+        //jsonUserData = data;
+        populateList(data);
+      },
+      error: function(response) {
+        alert("Prosimo poskusite kasneje!");
+        console.log(response);
+      }
+
+    });
+  }
+
+  loadPeople();
+});
