262c36eff084d3ebe2e0ee1001c9d9477cf6bb4a
MVolontar <44176749+MVolontar@users.noreply.github.com>
2019-03-18 21:20:56 +0100

    naknadno urejanje projekta - manjka shranjevanje v bazo

--- public/js/pages/project-list.js
+++ public/js/pages/project-list.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
+  window.value = [];
   function populateList(data) {
 
     console.log("Populating");
     $("#dropdownUporabniki").empty();
     var length = Object.keys(data).length;
     console.log(length);
+
+
     for (var i = 0; i < length; i++) {
+      window.value.push([data[i].id, data[i].username, data[i].name, data[i].surname]);
       $('#dropdownUporabniki').append($(`<option role='${data[i].id}' />`).val(data[i].username).text(data[i].username + ' (' + data[i].name + ' ' + data[i].surname + ')'));
     }
   }
